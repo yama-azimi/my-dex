2b52201fa883378334242b11104de795fb80ffa3
Mohammad Yama Azimi <mazimi.yama@gmail.com>
2023-07-31 18:52:20 +0430

    Implemented price chartt

--- src/components/App.js
+++ src/components/App.js
@@ -7,6 +7,7 @@ import Markets from './Markets';
 import Balance from './Balance';
 import Order from './Order';
 import OrderBook from './OrderBook';
+import PriceChart from './PriceChart';
 
 function App() {
 	const dispatch = useDispatch();
@@ -63,7 +64,7 @@ function App() {
 						<Order />
 					</section>
 					<section className='exchange__section--right grid'>
-						{/* PriceChart */}
+						<PriceChart />
 
 						{/* Transactions */}
 
--- src/store/selectors.js
+++ src/store/selectors.js
@@ -1,5 +1,5 @@
 import { createSelector } from 'reselect';
-import { get, groupBy, reject } from 'lodash';
+import { get, groupBy, reject, minBy, maxBy } from 'lodash';
 import { ethers } from 'ethers';
 import moment from 'moment';
 
@@ -101,4 +101,62 @@ const decorateOrderBookOrder = (order, tokens) => {
 		_orderTypeClass: orderType === 'buy' ? GREEN : RED,
 		_orderFillAction: orderType === 'buy' ? 'sell' : 'buy',
 	};
+};
+
+export const priceChartSelector = createSelector(filledOrders, tokens, (orders, tokens) => {
+	if (!tokens[0] || !tokens[1]) {
+		return;
+	}
+
+	// Filter orders by selected token.
+	orders = orders.filter((o) => o._tokenGet === tokens[0].address || o._tokenGet === tokens[1].address);
+	orders = orders.filter((o) => o._tokenGive === tokens[0].address || o._tokenGive === tokens[1].address);
+
+	// Sort orders by date ascending
+	orders = orders.sort((a, b) => a._timestamp - b._timestamp);
+	// Decorate orders
+	orders = orders.map((o) => decorateOrder(o, tokens));
+
+	// Get last two orders for final price & price change
+	let secondLastOrder, lastOrder;
+	[secondLastOrder, lastOrder] = orders.slice(orders.length - 2, orders.length);
+
+	// Get last order price
+	const lastPrice = get(lastOrder, '_tokenPrice', 0);
+
+	// Get second last order price
+	const secondLastPrice = get(secondLastOrder, '_tokenPrice', 0);
+
+	return {
+		lastPrice,
+		lastPriceChange: lastPrice >= secondLastPrice ? '+' : '-',
+		series: [
+			{
+				data: buildGraphData(orders),
+			},
+		],
+	};
+});
+
+const buildGraphData = (orders) => {
+	// Group orders by timestamp
+	orders = groupBy(orders, (o) => moment.unix(o._timestamp).startOf('hour').format());
+
+	const hours = Object.keys(orders);
+	const graphData = hours.map((hour) => {
+		// Fetch all orders from current hour
+		const group = orders[hour];
+
+		// Calulate price values: open, high, low, close
+		const open = group[0];
+		const high = maxBy(group, '_tokenPrice');
+		const low = minBy(group, '_tokenPrice');
+		const close = group[group.length - 1];
+
+		return {
+			x: new Date(hour),
+			y: [open._tokenPrice, high._tokenPrice, low._tokenPrice, close._tokenPrice],
+		};
+	});
+	return graphData;
 };
