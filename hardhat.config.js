b568987af742cfad5c277f88dcc4a08ab4546401
Mohammad Yama Azimi <mazimi.yama@gmail.com>
2023-08-29 00:07:14 +0430

    Prepare front-end for deploymenty

--- hardhat.config.js
+++ hardhat.config.js
@@ -2,19 +2,30 @@ require('@nomicfoundation/hardhat-toolbox');
 require('dotenv').config();
 require('@nomiclabs/hardhat-etherscan');
 
+const privateKeys = process.env.PRIVATE_KEYS || '';
+
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: '0.8.17',
+	solidity: '0.8.17',
 
-  network: {
-    localhost: {
-      URL: 'http://127.0.0.1:8545/',
-    },
-  },
+	networks: {
+		localhost: {
+			URL: 'http://127.0.0.1:8545/',
+		},
 
-  etherscan: {
-    // Your API key for Etherscan
-    // Obtain one at https://etherscan.io/
-    apiKey: 'YOUR_ETHERSCAN_API_KEY',
-  },
+		sepolia: {
+			url: `https://eth-sepolia.g.alchemy.com/v2/${process.env.ALCHEMY_SEPOLIA_API_KEY}`,
+			accounts: privateKeys.split(','),
+		},
+		goerli: {
+			url: `https://eth-goerli.g.alchemy.com/v2/${process.env.ALCHEMY_GOERLI_API_KEY}`,
+			accounts: privateKeys.split(','),
+		},
+	},
+
+	etherscan: {
+		// Your API key for Etherscan
+		// Obtain one at https://etherscan.io/
+		apiKey: 'YOUR_ETHERSCAN_API_KEY',
+	},
 };
